e5a9f4c9f19274d93e21b411d88d7763a9b5aa43
Moosems <95927277+Moosems@users.noreply.github.com>
2024-07-23 01:51:47 -0600

    Format

--- albero/__init__.py
+++ albero/__init__.py
@@ -2,6 +2,6 @@ from beartype.claw import beartype_this_package
 
 beartype_this_package()
 
-from .languages import get_lang, get_mapping  # noqa: F401
-from .tokens import Token  # noqa: F401
-from .tree_sitter import TreeSitterHighlighter  # noqa: F401
+from .languages import get_lang, get_mapping  # noqa: F401, E402
+from .tokens import Token  # noqa: F401, E402
+from .tree_sitter import TreeSitterHighlighter  # noqa: F401, E402
